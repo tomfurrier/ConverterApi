11613ce7b02aded9f8677252f0a5b4170c519742
Tamas Szucs <tamas@originbyte.io>
2023-01-17 02:10:42 +0100

    v1

---
+++ DocumentExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System.Xml.Linq;
+using System.Xml;
+
+namespace ConverterApi;
+
+public static class DocumentExtensions
+{
+    public static XmlDocument ToXmlDocument(this XDocument xDocument)
+    {
+        var xmlDocument = new XmlDocument();
+        using (var xmlReader = xDocument.CreateReader())
+        {
+            xmlDocument.Load(xmlReader);
+        }
+
+        return xmlDocument;
+    }
+
+    public static XDocument ToXDocument(this XmlDocument xmlDocument)
+    {
+        using (var nodeReader = new XmlNodeReader(xmlDocument))
+        {
+            nodeReader.MoveToContent();
+            return XDocument.Load(nodeReader);
+        }
+    }
+}
---
+++ SvgToPng.cs
@@ -0,0 +1,42 @@
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Svg;
+using System.Net.Http;
+using System.Drawing.Imaging;
+using System.Drawing;
+
+namespace ConverterApi;
+
+public static class SvgToPng
+{
+    [FunctionName("SvgToPng")]
+    public static async Task<IActionResult> Run(
+        [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req, ILogger log)
+    {
+        log.LogInformation("C# HTTP trigger function processed a request.");
+
+        var url = req.Query["url"];
+        _ = int.TryParse(req.Query["width"], out int width);
+        _ = int.TryParse(req.Query["height"], out int height);
+
+        using var httpClient = new HttpClient();
+        var svgString = await httpClient.GetStringAsync(url);
+        var svg = SvgDocument.FromSvg<SvgDocument>(svgString);
+        var bitmap = svg.Draw(width, height);
+        using var ms = new MemoryStream();
+        bitmap.Save(ms, ImageFormat.Png);
+
+        return new FileContentResult(ImageToByteArray(bitmap), "image/png");
+    }
+
+    private static byte[] ImageToByteArray(Image image)
+    {
+        var converter = new ImageConverter();
+        return (byte[])converter.ConvertTo(image, typeof(byte[]));
+    }
+}
